70829f5b241f86c82ba552f042fc2fc651362039
sunflower <17307130191@fudan.edu.cn>
2019-11-28 22:06:47 +0800

    rm obj

--- obj/kern/vm.d
+++
@@ -1,4 +0,0 @@
-obj/kern/vm.o: kern/vm.c inc/types.h inc/x86.h inc/mmu.h inc/string.h \
- inc/memlayout.h inc/error.h inc/assert.h inc/stdio.h inc/stdarg.h \
- kern/vm.h kern/proc.h kern/spinlock.h inc/elf.h kern/kalloc.h \
- kern/console.h kern/cpu.h
--- obj/lib/syscall.d
+++
@@ -1,3 +0,0 @@
-obj/lib/syscall.o: lib/syscall.c inc/syscall.h inc/lib.h inc/types.h \
- inc/stdio.h inc/stdarg.h inc/string.h inc/error.h inc/assert.h \
- inc/memlayout.h inc/traps.h
